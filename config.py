65fcb3f10fbd169031d1797dce1c31020ca4ea44
DiegoEYMadrid <101250499+DiegoEYMadrid@users.noreply.github.com>
2024-03-07 15:30:15 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+ 
+class Config:
+    AZURE_STORAGE_ACCOUNT = "formacioncloud"
+    AZURE_STORAGE_KEY ="DefaultEndpointsProtocol=https;AccountName=formacioncloud;AccountKey=GHUfsO5U+o9Q0sYjp1sbJ8b6FrY+521b1CFueGubx0xsYLscRSfMsKrwoZytDSwIdGXE9n4xa31p+AStAh0zrw==;EndpointSuffix=core.windows.net"
+    AZURE_STORAGE_USER_TABLE= "alumnos"
+   
